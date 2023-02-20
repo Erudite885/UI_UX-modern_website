4362316a71a57560824f4ae991cbaca4e846e4a0
Erudite885 <erudite885@gmail.com>
2023-02-20 16:35:15 +0100

    header component

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -3,8 +3,11 @@ import "./header.css";
 
 const Header = () => {
   return (
-    <div>
-      <h1>Header</h1>
+    <div className="gpt3__header section__padding" id="home">
+      <div className="gpt3__header-content"></div>
+      <h1 className="gradient__text">
+        Let's Build Something amazing with GPT-3 OpenAI
+      </h1>
     </div>
   );
 };
