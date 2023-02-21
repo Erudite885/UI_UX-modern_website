871aa78c1ec400fcaf77e75122a6f732fdcd851c
Erudite885 <erudite885@gmail.com>
2023-02-21 13:01:01 +0100

    more assets

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,9 +1,13 @@
 import logo from "./logo.svg";
+import face from './face.png';
+import people from './people.jpg'
 // import menu from "./menu.svg";
 // import close from "./close.svg";
 
 
 export { 
     logo, 
+    face, 
+    people,
     // menu, close 
 };
