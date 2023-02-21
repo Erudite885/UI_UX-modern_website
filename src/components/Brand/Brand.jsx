f51b2c99e54df5acade69a7cbce1992b7f95aae2
Erudite885 <erudite885@gmail.com>
2023-02-21 13:27:28 +0100

    brand component and stylng

--- src/components/Brand/Brand.jsx
+++ src/components/Brand/Brand.jsx
@@ -1,10 +1,24 @@
 import React from "react";
-import "./brand.css";
+import { google, dropbox, binance, atlas, airbnb } from "../../assets";
 
 const Brand = () => {
   return (
-    <div className="">
-      <h1>Brand</h1>
+    <div className="gpt3__brand flex flex-wrap justify-center items-center section__padding">
+      <div className="flex-1 max-w-[150px] min-w-[120px] m-4 flex justify-center items-center ">
+        <img src={google} alt="google logo" />
+      </div>{" "}
+      <div className="flex-1 max-w-[150px] min-w-[120px] m-4 flex justify-center items-center ">
+        <img src={binance} alt="binance logo" />
+      </div>{" "}
+      <div className="flex-1 max-w-[150px] min-w-[120px] m-4 flex justify-center items-center ">
+        <img src={dropbox} alt="dropbox logo" />
+      </div>{" "}
+      <div className="flex-1 max-w-[150px] min-w-[120px] m-4 flex justify-center items-center ">
+        <img src={atlas} alt="atlas logo" />
+      </div>{" "}
+      <div className="flex-1 max-w-[150px] min-w-[120px] m-4 flex justify-center items-center ">
+        <img src={airbnb} alt="airbnb logo" />
+      </div>
     </div>
   );
 };
