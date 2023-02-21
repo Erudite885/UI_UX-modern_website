d0a67a28fdcd31c2a4d347bbf37daca7fb612be9
Erudite885 <erudite885@gmail.com>
2023-02-21 13:10:15 +0100

    more assets

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,13 +1,20 @@
 import logo from "./logo.svg";
 import face from './face.png';
 import people from './people.jpg'
-// import menu from "./menu.svg";
-// import close from "./close.svg";
+import atlas from "./atlas.png";
+import airbnb from "./airbnb.png";
+import binance from "./binance.png";
+import dropbox from "./dropbox.png";
+import google from "./google.svg";
 
 
 export { 
     logo, 
     face, 
     people,
-    // menu, close 
+    atlas, 
+    airbnb,
+    binance,
+    dropbox,
+    google,
 };
