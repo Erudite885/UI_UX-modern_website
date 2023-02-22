b891fd822cbb822b9e23d78f16158ca024df66fc
Erudite885 <erudite885@gmail.com>
2023-02-22 12:15:53 +0100

    feature component styling

--- src/components/Feature/Feature.jsx
+++ src/components/Feature/Feature.jsx
@@ -1,10 +1,18 @@
 import React from "react";
 import "./feature.css";
 
-const Feature = () => {
+const Feature = ({ title, text }) => {
   return (
-    <div className="">
-      <h1>Feature</h1>
+    <div className="gpt3__features-container__feature w-full flex justify-between items-start flex-col md:m-4 my-4 lg:my-0 ">
+      <div className="gpt3__features-container__feature-title flex-1 max-w-[180px] mr-8 ">
+        <div className="w-[38px] h-[3px] bg-subtext mb-1 " />
+        <h1 className="font-poppins font-bold text-[24px] text-[#fff] ">
+          {title}
+        </h1>
+      </div>
+      <div className="gpt3__features-container__feature-text flex-2 max-w-[390px] flex ">
+        <p className='font-poppins font-medium text-[14px] text-text '>{text}</p>
+      </div>
     </div>
   );
 };
