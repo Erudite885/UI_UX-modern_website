9a781d52f004d44a4ebb901a3b66e0eaf05f6ab1
Erudite885 <erudite885@gmail.com>
2023-02-22 12:15:11 +0100

    whatgpt3 styling

--- src/container/WhatGPT3/WhatGPT3.jsx
+++ src/container/WhatGPT3/WhatGPT3.jsx
@@ -8,21 +8,39 @@ const WhatGPT3 = () => {
       className="gpt3__whatgpt3 section__margin bg-footer gradient__bg-what flex flex-col p-8 "
       id="wgpt3"
     >
-      <div className="gpt3__whatgpt3-feature flex ">
-        <Feature />
+      <div className="gpt3__whatgpt3-feature flex">
+        <Feature
+          title="What is GPT-3"
+          text=" Lorem ipsum dolor Lorem ipsum dolor sit amet, consectetur adip Lorem ipsum dolor sit
+          amet, consectetur adip.Lorem ipsum dolor sit amet, consectetur adip Lorem ipsum dolor sit
+          amet, consectetur adip. Lorem ipsum dolor sit amet, consectetur adip Lorem ipsum dolor sit
+          amet, consectetur adip. "
+        />
       </div>
-      <div className="gpt3__whatgpt3-heading flex flex-col md:flex-row justify-between md:items-start items-center mx-0 mt-8 mb-4 md:mb-0 ">
-        <h1 className="gradient__text font-poppins font-bold text-[28px] md:text-[34px] max-w-[510px] ">
+      <div className="gpt3__whatgpt3-heading flex flex-col sm:flex-row justify-between md:items-start items-center mx-0 mt-8 mb-4 md:mb-0 ">
+        <h1 className="gradient__text font-poppins font-bold text-[28px] md:text-[34px] max-w-[510px] lg:max-w-full lg:mb-8 ">
           The possibilities are beyond your imagination.
         </h1>
         <p className="font-poppins font-medium text-[16px] text-subtext cursor-pointer mt-5 md:mt-2 ">
           Explore the Library
         </p>
       </div>
-      <div className="gpt3__whatgpt3-container flex flex-wrap flex-row mt-2 ">
-        <Feature />
-        <Feature />
-        <Feature />
+      <div className="gpt3__whatgpt3-container flex flex-col lg:flex-row mt-2 ">
+        <Feature
+          title="Chatbots"
+          text=" Lorem ipsum dolor sit amet, consectetur adip Lorem ipsum dolor sit
+          amet, consectetur adip."
+        />
+        <Feature
+          title="Knowledgebase"
+          text=" Lorem ipsum dolor sit amet, consectetur adip Lorem ipsum dolor sit
+          amet, consectetur adip."
+        />
+        <Feature
+          title="Education"
+          text=" Lorem ipsum dolor sit amet, consectetur adip Lorem ipsum dolor sit
+          amet, consectetur adip."
+        />
       </div>
     </div>
   );
